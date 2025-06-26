a3f9e2118fcd4540408a1a73ac8df9bfea524da3
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-06-26 14:21:50 +0300

    first: first commit

---
+++ project/main/admin.py
@@ -0,0 +1,442 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.db.models import Count, Sum, Value, OuterRef, Subquery, F
+from django.db.models.functions import Coalesce
+from django.urls import path
+from django.template.response import TemplateResponse
+
+from datetime import datetime, timedelta
+
+from .models import Users, UTM, Orders
+from .views import custom_admin_view
+
+from rangefilter.filters import (
+    DateRangeFilterBuilder,
+    DateTimeRangeFilterBuilder,
+    NumericRangeFilterBuilder,
+    DateRangeQuickSelectListFilterBuilder,
+)
+
+
+class MyAdminSite(admin.AdminSite):
+
+    def dashboard_page(self, request):
+        return custom_admin_view(request, self=self)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        # print(urls)
+        custom_urls = [
+            path('dashboard/', admin.site.admin_view(self.dashboard_page), name='dashboard_page'),
+        ]
+        return custom_urls + urls
+
+    def get_app_list(self, request, app_label=None):
+        app_list = super().get_app_list(request, app_label)
+        if app_label is None or app_label == 'custom':
+            app_list.append(
+                {
+                    "name": "Общее",
+                    "app_label": "custom",
+                    "models": [
+                        {
+                            "name": "Статистика бота",
+                            "object_name": "dashboard",
+                            "admin_url": "/admin/dashboard",
+                            "view_only": True,
+                        }
+                    ],
+                }
+            )
+        return app_list
+    
+    
+
+admin.site = MyAdminSite()
+
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
+
+
+class CustomDateTimeFilter(admin.SimpleListFilter):
+    title = 'Фильтры по дате'
+    parameter_name = 'custom_date_filter'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('today', 'Сегодня'),
+            ('yesterday', 'Вчера'),
+            ('this_week', 'В течении 7 дней'),
+            ('this_month', 'В этом месяце'),
+            ('this_year', 'В этом году'),
+            ('date_exists', 'Дата указана'),
+            ('date_not_exists', 'Дата не указана'),
+        )
+
+    def queryset(self, request, queryset):
+        today = datetime.now()
+        # print(today)
+        if self.value() == 'today':
+            start_of_today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+            end_of_today = today.replace(hour=23, minute=59, second=59, microsecond=999999)
+            return queryset.filter(time_create__range=(start_of_today, end_of_today))
+        elif self.value() == 'yesterday':
+            start_of_yesterday = today.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+            end_of_yesterday = today.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(seconds=1)
+            return queryset.filter(time_create__range=(start_of_yesterday, end_of_yesterday))
+        elif self.value() == 'this_week':
+            # start_of_week = today - timedelta(days=today.weekday())
+            start_of_week = today - timedelta(days=6, hours=23, minutes=59, seconds=59)
+
+            # end_of_week = start_of_week + timedelta(days=6, hours=23, minutes=59, seconds=59)
+            end_of_week = today
+
+            return queryset.filter(time_create__range=(start_of_week, end_of_week))
+        elif self.value() == 'this_month':
+            start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+            end_of_month = (start_of_month + timedelta(days=32)).replace(day=1) - timedelta(seconds=1)
+            return queryset.filter(time_create__range=(start_of_month, end_of_month))
+        elif self.value() == 'this_year':
+            start_of_year = today.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+            end_of_year = today.replace(month=12, day=31, hour=23, minute=59, second=59, microsecond=999999)
+            return queryset.filter(time_create__range=(start_of_year, end_of_year))
+        elif self.value() == 'date_exists':
+            return queryset.exclude(time_create__isnull=True)
+        elif self.value() == 'date_not_exists':
+            return queryset.filter(time_create__isnull=True)
+        
+        return queryset
+    
+
+# class UTMSourceFilter(admin.filters.SimpleListFilter):
+#     title = 'Кастомный UTM фильтр'
+#     parameter_name = 'utm_source_start'
+
+#     def lookups(self, request, model_admin):
+#         request_session = request.session
+
+#         # Получаем уникальные значения начала строки
+#         # utm_source_start = request.GET.get('utm_source_start')
+#         print('lookup',request.GET)
+#         print('request session', request_session.__dict__)
+
+#         if request_session.get('prefix_utm') and \
+#             request_session.get('second_part_utm'):
+#             print('22')
+
+#         utm_source = self.value()
+#         print(utm_source)
+#         if not utm_source:
+#             prefixes = Users.objects.filter(utm_source__isnull=False)\
+#                                     .values_list('utm_source', flat=True)\
+#                                     .distinct()
+
+#             unique_prefix =  [('_'.join(prefix.split('_')[:2]), '_'.join(prefix.split('_')[:2])) \
+#                                 for prefix in set(prefixes) if prefix is not None]
+            
+#             request_session['prefix_utm'] = None
+#             request_session['second_part_utm'] = None
+
+#             return sorted(set(unique_prefix))
+
+
+#             # return [('_'.join(prefix.split('_')[:2]), '_'.join(prefix.split('_')[:2])) \
+#             #         for prefix in set(prefixes) if prefix is not None]
+#         else:
+#             if utm_source[:2].isdigit():
+#                 prefix_utm = request_session.get('prefix_utm')
+#                 prefixes = Users.objects.filter(utm_source__isnull=False,
+#                                                 utm_source__endswith=utm_source,
+#                                                 utm_source__startswith=prefix_utm)\
+#                                         .values_list('utm_source', flat=True)\
+#                                         .distinct()
+                
+#                 request.session['second_part_utm'] = utm_source
+
+#             check_value = (utm_source[:2].isdigit()) or (utm_source == '--')
+            
+#             if not check_value:
+#                 prefixes = Users.objects.filter(utm_source__isnull=False,
+#                                                 utm_source__startswith=utm_source)\
+#                                         .values_list('utm_source', flat=True)\
+#                                         .distinct()
+                
+#                 unique_prefix = [('_'.join(prefix.split('_')[2:]), '_'.join(prefix.split('_')[2:])) \
+#                                  for prefix in set(prefixes) if prefix is not None]
+                
+#                 request.session['prefix_utm'] = utm_source
+                
+#                 return sorted(set(unique_prefix))
+
+#                 # return [('_'.join(prefix.split('_')[2:]), '_'.join(prefix.split('_')[2:])) \
+#                 #         for prefix in set(prefixes) if prefix is not None]
+#             else:
+#                 # request.session['prefix_utm'] = None
+#                 return [('--', '--')]
+#         # return [(prefix, prefix) for prefix in set(prefixes) if prefix is not None]
+
+
+#     def queryset(self, request, queryset):
+#         request_session = request.session
+        
+#         # if any(key for key in request.GET if key.startswith('time_create')):
+#         #     request.GET['time_create__isnull'] = ['False']
+#         # else:
+#         #     try:
+#         #         del request.GET['time_create__isnull']
+#         #     except Exception as ex:
+#         #         print(ex)
+#         print(request.GET)
+#         print('request session', request_session.__dict__)
+#         # utm_source_start = request.GET.get('utm_source_start')
+#         utm_source = self.value()
+#         # print(utm_source)
+#         # print(22)
+#         if utm_source:
+#         #     # if not utm_source_start:
+#             check_value = utm_source[:2].isdigit()
+            
+#             if not check_value:
+#                 queryset = queryset.filter(utm_source__startswith=utm_source,
+#                                            utm_source__isnull=False)
+#             else:
+#                 prefix_utm = request_session.get('prefix_utm')
+#                 if prefix_utm:
+#                     queryset = queryset.filter(utm_source__startswith=prefix_utm,
+#                                             utm_source__endswith=utm_source,
+#                                             utm_source__isnull=False)
+                    
+#                     # request.session['prefix_utm'] = None
+
+#                 # utm_source_start = utm_source_start[0]
+#                 # queryset = queryset.filter(utm_source__startswith=utm_source_start)
+#         # print(queryset)
+#         return queryset
+
+
+class UTMInline(admin.StackedInline):
+    model = UTM
+    extra = 0
+    classes = [
+        'collapse',
+        ]
+
+    def has_change_permission(self, request, obj = ...):
+        return False
+
+#Отображение комментариев в админ панели
+# @admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'get_utm_source',
+        'utm__utm_campaign',
+        # 'product_count',
+        # 'product_all_time_count',
+        'time_create',
+    )
+    inlines = [UTMInline]
+
+    def get_utm_source(self, obj):
+        return obj.utm_source if obj.utm_source and (obj.utm_source.find('_') != 1) else obj.utm.source
+    
+    get_utm_source.short_description = 'UTM источник'
+
+    readonly_fields = (
+        'username',
+        'first_name',
+        'last_name',
+        'utm_source',
+        'time_create',
+        # 'product_count',
+        # 'product_all_time_count',
+    )
+
+    ordering = (
+        '-time_create',
+    )
+
+    list_filter = (
+        CustomDateTimeFilter,
+        ("time_create", DateRangeFilterBuilder()),
+        )
+    
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ['username',
+                           "first_name",
+                           "last_name",
+                        #    "subscription",
+                           "utm_source",
+                        #    "product_count",
+                        #    "product_all_time_count",
+                        #    'related_utm',
+                           "time_create"]
+            },
+        ),
+    ]
+
+    # def product_count(self, obj):
+    #     wb_products = obj.wb_product_count if obj.wb_product_count else 0
+    #     ozon_products = obj.ozon_product_count if obj.ozon_product_count else 0
+
+    #     product_count = wb_products + ozon_products
+
+    #     return f'{product_count} | wb: {wb_products} | ozon: {ozon_products}'
+    
+    # def product_all_time_count(self, obj):
+    #     wb_products = obj.wb_total_count
+    #     ozon_products = obj.ozon_total_count
+
+    #     product_count = wb_products + ozon_products
+
+    #     return f'{product_count} | wb: {wb_products} | ozon: {ozon_products}'
+
+    # product_count.short_description = 'Число продуктов'
+    # product_count.admin_order_field = 'all_product_count'
+
+    # product_all_time_count.short_description = 'Число продуктов за всё время'
+    # product_all_time_count.admin_order_field = 'all_product_count'
+    
+admin.site.register(Users, UsersAdmin)
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'request_type',
+        'time_create',
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
+    
+admin.site.register(Orders, OrdersAdmin)
+# @admin.register(UserProducts)
+# class UserProductsAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'user',
+#         'product_name',
+#         'product_marker',
+#         'time_create',
+#     )
+
+#     list_filter = (
+#         # 'product__name',
+#         'product__product_marker',
+#         'user',
+#         'time_create',
+#     )
+
+#     ordering = (
+#         '-time_create',
+#     )
+
+#     def product_name(self, obj):
+#         return obj.product.name
+    
+#     product_name.short_description = 'Название продукта'
+
+#     def product_marker(self, obj):
+#         return obj.product.product_marker
+    
+#     product_marker.short_description = 'Маркетплейс'
+
+#     def get_queryset(self, request):
+#         return super().get_queryset(request).select_related('product', 'user')
+    
+#     def has_change_permission(self, request, obj = ...):
+#         return False
+        # return super().has_change_permission(request, obj)
+
+# @admin.register(UTM)
+# class UTMAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'pretty_user',
+#         'source',
+#     )
+
+#     def pretty_user(seld, obj):
+#         return f'{obj.user.tg_id} {obj.user.username}'
+    
+#     def get_queryset(self, request):
+#         return super().get_queryset(request).select_related('user')
+    
+# admin.site.register(UTM, UTMAdmin)
+
+# class CategoryChannelLinkInline(admin.TabularInline):
+#     model = CategoryChannelLink
+#     extra = 1
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name',
+#     )
+#     inlines = [
+#         CategoryChannelLinkInline,
+#     ]
+
+# admin.site.register(Category, CategoryAdmin)
+
+
+# class ChannelLinkAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name',
+#     )
+
+# admin.site.register(ChannelLink, ChannelLinkAdmin)
+
+# Переопределяем метод get_urls стандартного admin.site
+# custom_admin_urls =  [
+#         path('dashboard/', admin.site.admin_view(custom_admin_view), name='custom_admin_view'),
+#         ]
+# admin_urls = admin.site.get_urls()
+
+# def get_admin_urls():
+#     custom_urls = [
+#         path('dashboard/', admin.site.admin_view(custom_admin_view), name='custom_admin_view'),
+#     ]
+
+#     return custom_urls + admin_urls
+
+# admin.site.get_urls = lambda: get_admin_urls()
+
+
+# class MyAdminSite(admin.AdminSite):
+#     # site_header = "Custom Administration"
+#     def get_urls(self):
+#         urls = super().get_urls()
+#         # print(urls)
+#         custom_urls = [
+#             path('dashboard/', admin.site.admin_view(custom_admin_view), name='custom_admin_view'),
+#         ]
+#         return custom_urls + urls
+
+#     def get_app_list(self, request, app_label=None):
+#         app_list = super().get_app_list(request, app_label)
+#         if app_label is None or app_label == 'custom':
+#             app_list.append(
+#                 {
+#                     "name": "Custom",
+#                     "app_label": "custom",
+#                     "models": [
+#                         {
+#                             "name": "Dashboard",
+#                             "object_name": "dashboard",
+#                             "admin_url": "/admin/dashboard",
+#                             "view_only": True,
+#                         }
+#                     ],
+#                 }
+#             )
+#         return app_list
+
+#     def dashboard_view(self, request):
+#         context = dict(self.each_context(request))
+#         # context['parameters'] = config.get_parameters()
+#         return TemplateResponse(request, 'admin/dashboard/custom_admin.page.html', context)
+    
+
+# admin.site = MyAdminSite()
---
+++ project/main/models.py
@@ -0,0 +1,220 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AlembicVersion(models.Model):
+    version_num = models.CharField(primary_key=True, max_length=32)
+
+    class Meta:
+        managed = False
+        db_table = 'alembic_version'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Users(models.Model):
+    tg_id = models.BigAutoField(primary_key=True)
+    username = models.CharField(blank=True, null=True)
+    first_name = models.CharField(blank=True, null=True)
+    last_name = models.CharField(blank=True, null=True)
+    time_create = models.DateTimeField('Время добавления',
+                                       blank=True,
+                                       null=True)
+    last_login_time = models.DateTimeField(blank=True, null=True)
+    utm_source = models.CharField('UTM метка',
+                                  blank=True,
+                                  null=True)
+    is_active = models.BooleanField('Активность', default=True)
+    class Meta:
+        managed = False
+        db_table = 'users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'Пользователь - ID {self.tg_id} {self.username}'
+    
+
+
+class Orders(models.Model):
+
+    request_type_chioces = [
+        ('Сотрудничество', 'Сотрудничество'),
+        ('Сообщить об ошибке', 'Сообщить об ошибке'),
+    ]
+    request_type = models.CharField('Тип обращения',
+                                    max_length=255,
+                                    choices=request_type_chioces)
+    comment = models.TextField('Комментарий к обращению')
+    user = models.ForeignKey(Users,
+                             on_delete=models.CASCADE,
+                             verbose_name='Пользователь')
+    time_create = models.DateTimeField('Время добавления',
+                                       blank=True,
+                                       null=True)
+    class Meta:
+        managed = False
+        db_table = 'orders'
+        verbose_name = 'Обращение'
+        verbose_name_plural = 'Обращения'
+
+    def __str__(self):
+        return f'Обращение от {self.time_create}, юзер {self.user}'
+
+
+class UTM(models.Model):
+    keitaro_id = models.CharField(max_length=255, blank=True, null=True)
+    utm_source = models.CharField(max_length=255, blank=True, null=True)
+    utm_medium = models.CharField(max_length=255, blank=True, null=True)
+    utm_campaign = models.CharField(max_length=255, blank=True, null=True)
+    utm_content = models.CharField(max_length=255, blank=True, null=True)
+    utm_term = models.CharField(max_length=255, blank=True, null=True)
+    banner_id = models.CharField(max_length=255, blank=True, null=True)
+    campaign_name = models.CharField(max_length=255, blank=True, null=True)
+    campaign_name_lat = models.CharField(max_length=255, blank=True, null=True)
+    campaign_type = models.CharField(max_length=255, blank=True, null=True)
+    campaign_id = models.CharField(max_length=255, blank=True, null=True)
+    creative_id = models.CharField(max_length=255, blank=True, null=True)
+    device_type = models.CharField(max_length=255, blank=True, null=True)
+    gbid = models.CharField(max_length=255, blank=True, null=True)
+    keyword = models.CharField(max_length=255, blank=True, null=True)
+    phrase_id = models.CharField(max_length=255, blank=True, null=True)
+    coef_goal_context_id = models.CharField(max_length=255, blank=True, null=True)
+    match_type = models.CharField(max_length=255, blank=True, null=True)
+    matched_keyword = models.CharField(max_length=255, blank=True, null=True)
+    adtarget_name = models.CharField(max_length=255, blank=True, null=True)
+    adtarget_id = models.CharField(max_length=255, blank=True, null=True)
+    position = models.CharField(max_length=255, blank=True, null=True)
+    position_type = models.CharField(max_length=255, blank=True, null=True)
+    source = models.CharField(max_length=255, blank=True, null=True)
+    source_type = models.CharField(max_length=255, blank=True, null=True)
+    region_name = models.CharField(max_length=255, blank=True, null=True)
+    region_id = models.CharField(max_length=255, blank=True, null=True)
+    yclid = models.CharField(max_length=255, blank=True, null=True)
+    client_id = models.CharField(max_length=255, blank=True, null=True)
+    user = models.OneToOneField(Users, on_delete=models.CASCADE, related_name='utm')
+    
+    class Meta:
+        db_table = 'utms'
+        managed = False
+
+    def __str__(self):
+        return f'{self.user.tg_id} {self.keitaro_id} {self.source}'
