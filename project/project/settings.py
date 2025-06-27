6589fabf380f45a84d4aba482d2e498208a4a965
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-06-27 15:53:21 +0300

    feat: add new admin models for mass send

--- project/project/settings.py
+++ project/project/settings.py
@@ -204,7 +204,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # SITE_DOMAIN = PUBLIC_URL
-SITE_DOMAIN = '127.0.0.1:8000'
+SITE_DOMAIN = '65.108.242.208:8001'
 
 PROTOCOL = 'http://'
 
