988852b264ed757af8ddbc6083ca12ed6135d4c4
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-06-30 10:26:57 +0300

    fix: edit custom js file

--- project/main/static/main/js/test_1.js
+++ project/main/static/main/js/test_1.js
@@ -10,19 +10,22 @@ window.addEventListener('load', function () {
 
     console.log(is_detail_view);
     if (is_detail_view != 0) {
+        if (window.location.pathname.includes('/change/')) {
+    
+            let main_div = document.getElementById('content-main');
+            // let new_li = document.createElement('li');
+            let mass_send_button = document.createElement('button');
+            mass_send_button.textContent = 'mass_send';
+            mass_send_button.onclick = function() {
+                let data = fetch(`http://65.108.242.208/send_mass_message?name_send=${name}`)
+                .then(resp => {
+                    resp.text().then(console.log)
+                })
+                alert("Рассылка запущена")
+            };
+            main_div.appendChild(mass_send_button);
 
-        let main_div = document.getElementById('content-main');
-        // let new_li = document.createElement('li');
-        let mass_send_button = document.createElement('button');
-        mass_send_button.textContent = 'mass_send';
-        mass_send_button.onclick = function() {
-            let data = fetch(`http://65.108.242.208/send_mass_message?name_send=${name}`)
-            .then(resp => {
-                resp.text().then(console.log)
-            })
-            alert("Рассылка запущена")
-        };
-        main_div.appendChild(mass_send_button);
+        }
         // main_ul.append(new_li);
     }
 })
