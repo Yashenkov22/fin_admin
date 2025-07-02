7b69278c3fd2d0e56b918288df3b89a330a22d82
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-07-02 15:50:09 +0300

    test: test with name param

--- project/main/utils/signals.py
+++ project/main/utils/signals.py
@@ -1,7 +1,7 @@
 import aiohttp
 
-async def run_background_task_with_delay(obj_id: int):
-    _url = f'http://65.108.242.208/run_background_task_with_delay?obj_id={obj_id}'
+async def run_background_task_with_delay(send_name: str):
+    _url = f'http://65.108.242.208/run_background_task_with_delay?send_name={send_name}'
     timeout = aiohttp.ClientTimeout(total=5)
     async with aiohttp.ClientSession() as session:
         async with session.get(_url,
