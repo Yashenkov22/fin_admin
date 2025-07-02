1be167705a7058045ad1ccb26712bf9c56863c06
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-07-02 17:26:49 +0300

    test: test transaction in signal

--- project/main/admin.py
+++ project/main/admin.py
@@ -62,6 +62,10 @@ class MassSendMessageAdmin(SummernoteModelAdmin):
         MassSendFileStacked,
     ]
 
+    readonly_fields = (
+        'has_delayed_task',
+    )
+
     fieldsets = [
         (
             None,
